4f734d7ebecaf082f7a01a480e3c8e667b9fce1f
Hassan Mkhallati <hmkhalla@ulb.ac.be>
2019-10-30 13:07:09 +0100

    Push de l'exécutable finale, erreur syntaxe

--- kotok.c
+++ kotok.c
@@ -11,35 +11,32 @@ int main(int argc, char**argv){
 	FILE* fin = NULL;
 	FILE* out = NULL;
 	structArray* table = NULL;
-	else
+	const char* IPATH = argv[1];
+	const char* OPATH = argv[2];
+	if ((fin = fopen(IPATH, "r")))
 	{
-		const char* IPATH = argv[1];
-		const char* OPATH = argv[2];
-		if ((fin = fopen(IPATH, "r")))
+		table = init_memory();
+		for (int i=0; fscanf(fin, "%d %d %f", &x,&y,&z) != EOF; i++)
 		{
-			table = init_memory();
-			for (int i=0; fscanf(fin, "%d %d %f", &x,&y,&z) != EOF; i++)
-			{
-				if (table == NULL || add_player(table, x, z) == NULL || add_player(table, y, fabsf(z-1)) == NULL )
-				{//fermeture du fichier in avant de quitter le programme à cause d'une erreur d'allocation ou de réallocation de mémoire
-					fclose(fin);
-					return EXIT_FAILURE;
-				}
-			};
-			fclose(fin);
-			if((out = fopen(OPATH, "w")))
-			{
-				store(out, table);
-				fclose(out);
-			}
-			else
-			{// erreur d'ouverture du fichier out
-				delete_table(table);
+			if (table == NULL || add_player(table, x, z) == NULL || add_player(table, y, fabsf(z-1)) == NULL )
+			{//fermeture du fichier in avant de quitter le programme à cause d'une erreur d'allocation ou de réallocation de mémoire
+				fclose(fin);
 				return EXIT_FAILURE;
-			} 
-			delete_table(table);
-			return EXIT_SUCCESS;
+			}
+		};
+		fclose(fin);
+		if((out = fopen(OPATH, "w")))
+		{
+			store(out, table);
+			fclose(out);
 		}
-		else  return EXIT_FAILURE;	// erreur d'ouverture du fichier in
+		else
+		{// erreur d'ouverture du fichier out
+			delete_table(table);
+			return EXIT_FAILURE;
+		} 
+		delete_table(table);
+		return EXIT_SUCCESS;
 	}
+	else  return EXIT_FAILURE;	// erreur d'ouverture du fichier in
 }
